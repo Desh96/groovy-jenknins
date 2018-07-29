9df30be1a8217df83090977f5e69e183ae2d0271
manukevich <manukevich@localhost.localdomain>
2018-07-29 22:15:06 +0300

    task1.push_pull

---
+++ task9.1.groovy
@@ -0,0 +1,27 @@
+def nexus_path = "192.168.1.2:8081"
+def group = "project"
+def artifact = "helloworld"
+def version = "1.${BUILD_NUMBER}"
+def repo = "jenkins-repo"
+def com = "push" 
+def log_pass = "admin:admin123"
+
+
+if("$com"=="push"){
+    def File = new File ("/tmp/${artifact}-${version}.tar.gz").getBytes()
+    def con = new URL("http://${nexus_path}/repository/${repo}/${group}/${artifact}/${version}/${artifact}-${version}.tar.gz").openConnection()
+    con.setRequestMethod("PUT")
+    con.doOutput = true
+    con.setRequestProperty( "Content-Type", "application/x-gzip" )
+    def writer = new DataOutputStream(con.outputStream)
+    writer.write(File)
+    writer.close()
+    println con.responseCode
+}
+
+else {
+    new File("/tmp/${ART_ID}-${version}.tar.gz").withOutputStream { out ->
+        def url = new URL("http://${nexus_path}/repository/${repo}/${group}/${artifact}/${VER}/${artifact}-${version}.tar.gz").openConnection()
+        out << url.inputStream
+    }
+}
